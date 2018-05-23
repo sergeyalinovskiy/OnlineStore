2d77dc13cca23755f4b12ce5b24c1f7b64c8297d
sergeyalinovskiy <sergey713824@gmail.com>
2018-05-23 22:19:24 +0300

    HF-1.2: add bitArray for picture in srvice and add PictureConverter in common

--- OnlineStore_Epam2018/SA.OnlineStore.WCF/MakingPublicityList.cs
+++ OnlineStore_Epam2018/SA.OnlineStore.WCF/MakingPublicityList.cs
@@ -1,5 +1,6 @@
 ﻿namespace SA.OnlineStore.WCF
 {
+    using SA.OnlineStore.Common.Convert;
     #region Usings
     using System;
     using System.Collections.Generic;
@@ -15,70 +16,70 @@
                     Id = 1,
                     Name = "Реклама_1",
                     Text = "Текст_1",
-                    Picture="2.jpg"
+                    Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\2.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 2,
                     Name = "Реклама_2",
                     Text = "Текст_2",
-                    Picture="4.jpg"
+                     Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\4.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 3,
                     Name = "Реклама_3",
                     Text = "Текст_3",
-                    Picture="5.jpg"
+                     Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\5.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 4,
                     Name = "Реклама_4",
                     Text = "Текст_4",
-                    Picture="6.jpg"
+                     Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\6.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 5,
                     Name = "Реклама_5",
                     Text = "Текст_5",
-                    Picture="7.jpg"
+                     Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\7.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 6,
                     Name = "Реклама_6",
                     Text = "Текст_6",
-                    Picture="8.jpg"
+                    Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\8.jpg"))
                 },
                 new PublicityService()
         {       
                     Id = 7,
                     Name = "Реклама_7",
                     Text = "Текст_7",
-                    Picture="9.jpg"
+                     Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\9.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 8,
                     Name = "Реклама_8",
                     Text = "Текст_8",
-                    Picture="2.jpg"
+                     Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\2.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 9,
                     Name = "Реклама_9",
                     Text = "Текст_9",
-                    Picture="5.jpg"
+                    Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\5.jpg"))
                 },
                 new PublicityService()
         {
                     Id = 10,
                     Name = "Реклама_10",
                     Text = "Текст_10",
-                    Picture="8.jpg"
+                     Picture=PictureConverter.ImageToByteArray(PictureConverter.GetImg("D:\\111\\OnlineStore\\OnlineStore_Epam2018\\SA.OnlineStore.WCF\\Content\\Images\\8.jpg"))
                 }
 
             };
